cd95f344ba3e00dcba59e655cce5578ca2de674b
Vincent Zaldivar <vincentzaldivar@Vincents-Mac-mini.local>
2020-04-15 08:40:06 -0700

    Fix Sensor.py compile errors & add new imports to install.py

--- v2020.0/Sensor.py
+++ v2020.0/Sensor.py
@@ -1,6 +1,44 @@
-import time
+#!/usr/bin/env python
 
-from MissionControl import *
+__author__ =  "Blaze Sanders"
+__email__ =   "blaze.d.a.sanders@gmail.com"
+__company__ = "Robotic Beverage Technologies Inc"
+__status__ =  "Development"
+__date__ =    "Late Updated: 2020-04-15"
+__doc__ =     "Class to communicate with all sensors inside the Tapomatic kiosk" 
+
+# Allow program to pause operation and create local timestamps
+from time import sleep
+
+# Allow communication (Read and write) to senors via the serial port 
+#TODO if gpiozero doesn't support import serial
+
+# Allow communication between one or more kiosks with central server
+#TODO from MissionControl import *
+
+try:
+	# The following imports do NOT work in a Mac or PC dev enviroment 
+	# But are needed for a Raspberry Pi controlled kiosk
+
+	# Allow control of input and output devices such as Sensors
+	from gpiozero import InputDevice, OutputDevice, Energenie
+	
+	# Allow control of  HC-SR04 ultrasonic distance sensor (See CamJam #3 EduKit)
+	# http://camjam.me/?page_id=1035
+	from gpiozero import DistanceSensor
+
+except ImportError:
+	
+	# Allow BASH command to be run inside Python3 code like this file
+	import subprocess
+	from subprocess import Popen, PIPE
+	from subprocess import check_call
+
+	# This import SHOULD work on both Mac & PC to allow software dev work (no hardware in the loop testing)
+	check_call("pip3 install gpiozero pigpio", shell=True)
+
+	DebugObject = Debug(True)
+	DebugObject.Dprint(DebugObject, "WARNING: You are running code on Mac or PC (NOT a Raspberry Pi)")
 
 class Sensor():	
 
@@ -15,31 +53,36 @@ class Sensor():
 	# TODO Define all pins in schematic
 	PING_Xaxis_Pin = 12
 	PING_Xaxis_Pin_Name = "Board12"
-    PING_Yaxis_Pin = 13
+	PING_Yaxis_Pin = 13
 	PING_Yaxis_Pin_Name = "Board13"
-    PING_Zaxis_Pin = 14
+	PING_Zaxis_Pin = 14
 	PING_Zaxis_Pin_Name = "Board14"
 	
 	def __init__(self, currentNumOfSensors, sType, pins, partNumber):
-			wires = numpy.empty(len(pins), dtype=object)   # TODO wires = ndarray((len(pins),),int) OR wires = [None] * len(pins) 				# Create an array on same length as pins[?, ?, ?]
-			for i in pins:
-				self.wires[i] = pins[i]
-			self.sensorType = sType
-			currentNumOfActuators += 1
-			self.sensorID = currentNumOfSensors# Auto-incremented interger class variable
-			self.partNumber = partNumber
+		wires = numpy.empty(len(pins), dtype=object)   # TODO wires = ndarray((len(pins),),int) OR wires = [None] * len(pins) 				# Create an array on same length as pins[?, ?, ?]
+		for i in pins:
+			self.wires[i] = pins[i]
+		self.sensorType = sType
+		currentNumOfActuators += 1
+		self.sensorID = currentNumOfSensors# Auto-incremented interger class variable
+		self.partNumber = partNumber
 		
 	def StartFullDuplexSerial():
-		Serial.
+		print("TODO")
+		#Serial.
 		
 	def SendSerialCommand():
+		print("TODO")
 	
 	def ReceiveSerialCommand():
+		print("TODO")
 		
 	def StartI2C():
-		gpio. 
+		print("TODO")
+		#gpio. 
 		
 	def SendI2C():
+		print("TODO")
 
 	
 	def GetLevel(self, lType):
@@ -68,7 +111,8 @@ class Sensor():
 		percentage = GetForce(lType)/liquidWeightAt100percent
 
 		if(percentage < 10.0):
-			MissionControl.SendLiquidLevelMessage(lType,  MissionControl.MESSAGE_1)
+			print("TODO")
+			#MissionControl.SendLiquidLevelMessage(lType,  MissionControl.MESSAGE_1)
     
 		return percentage
 
@@ -80,23 +124,23 @@ class Sensor():
 
 		@link https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
 		
+		@link https://github.com/aguegu/ardulibs/
+		
 		Degraw 5kg Load Cell and HX711 Combo Pack Kit - Load Cell Amplifier ADC Weight Sensor for Arduino Scale - Everything Needed for Accurate Force Measurement https://www.amazon.com/dp/B075317R45/ref=cm_sw_r_cp_api_i_Ph7JEb5A1F12M
 
-https://github.com/aguegu/ardulibs/
-		
-		 
 		return forceInNewtons - The weight of liquid in Newtons
 		"""
+
 		forceSensorID = FindLiquidForceSensor(lType)
 
-		SCK = 
-		DT = 
+		print("TODO")
+		#SCK = 
+		#DT = 
 
 		forceInlbs = DT 
 		forceInNewtons = forceInlbs * 0.2248
 		
-
-    	return forceInNewtons
+		return forceInNewtons
 
 	def FindLiquidForceSensor(lType):
 		
@@ -124,8 +168,8 @@ https://github.com/aguegu/ardulibs/
 
     	    return status
     	
-    def GetLASERpingDistance(pingPinNumber):
-    	range := Ping.GetMillimeters(pingPinNumber) 
-    	    
-    	return range 
+	def GetLASERpingDistance(pingPinNumber):
+		range = Ping.GetMillimeters(pingPinNumber) 
+		
+		return range 
    
