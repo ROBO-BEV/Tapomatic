b4be6dc16a2df527d4205f84b574778e3fd8498a
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-04-17 21:13:27 -0700

    Add function definitions to define Mission Control data

--- v2020.0/MissionControl.py
+++ v2020.0/MissionControl.py
@@ -1,6 +1,31 @@
 import time
 
 
+KIOSK.ID = 
+
 class MissionControl():	
 
-def __init__(self, currentNumOfSensors, sType, pins, partNumber):
+    def __init__(self, currentNumOfSensors, currentNumOfActuators, kioskID, version):
+    
+    def ReportLiquidLevel(lType, percentage, kioskID):
+		
+	def ReportLowLiquidLevel(lType, bottleLocation, kioskID):
+    
+	def ReportDamage():
+	
+	def ReportPowerState():
+	
+	def ReportCoconutUsage():
+	
+	def ReportTapUsage():
+	
+	def ReportKnifeStatus()
+
+	def StartOTA():
+		#TapomaticCodeBaseZone1
+	
+		#TapomaticCodeBaseZone2
+	
+	def StopOTA():
+	
+	
