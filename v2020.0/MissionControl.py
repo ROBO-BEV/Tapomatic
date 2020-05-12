713fa4b3b35d8e4ec9fa18e0ba3a2d2ebe8874b2
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-05-12 04:55:03 -0700

    WIP: Added comments and CSV reader

--- v2020.0/MissionControl.py
+++ v2020.0/MissionControl.py
@@ -5,35 +5,51 @@ __author__  = "Blaze Sanders"
 __email__   = "blaze.d.a.sanders@gmail.com"
 __company__ = "Robotic Beverage Technologies, Inc"
 __status__  = "Development"
-__date__    = "Late Updated: 2020-05-11"
+__date__    = "Late Updated: 2020-05-12"
 __doc__     = "Class to define OTA commuications architecture of 30K+ Tapomatic kiosk"
 """
 
 # Allow program to create GMT and local timestamps
 from time import gmtime, strftime
 
+# Allow program to READ Comma Separated Value files
+import csvreader 
+
 LOW_LIQUID_MESSAGE = 0
 PHYSICAL_DAMAGE_MESSAGE = 1
 LOW_POWER_MESSAGE = 2
 DULL_KNIFE_MESSAGE = 3
+VERISON_MESSAGE = 4
 
 
 class MissionControl():	
 
+	totalCoconutsVended = 0
+	totalTapsUsed = 0
+	
+
     def __init__(self, kioskID, version, key):
 		"""
-
+		
+		Key arguments:
+		kioskID -- Unique ID for every prototype or production Tapomatic manufactured
+		version -- Verison on software that should be running on a Tapomatic
+		key -- Security key for ALL Tapoamtics to allow Over-The-Air (OTA) updates
+		
+		Return value:
+		Newly created MissionControl() object
 		"""
 
         self.DebugObject = Debug(True)
 
 		self.kioskID = kioskID    
 		self.version = version
-		self.key = key 			# For example Coco420@69 taken from a hidden .key textfile 
+		self.key = key 			
 
     def ReportLiquidLevel(lType, internalBottleLocation, kioskID):
     	"""
-    	Report 
+    	Report the current liquid level as perfectage 
+    	
     	"""
     	
     	
@@ -41,9 +57,16 @@ class MissionControl():
     	
     	
     def ReportLowLiquidLevel(lType, internalBottleLocation, kioskID):
+    	"""
+    	High priority alert that the current liquid level is below 20%
+    	"""
     
-    def GetKioskGPSlocation():
+    def GetKioskGPSlocation(kioskID):
+    	"""
+    	Update to RavenDB Implemention in August 20200
+    	"""
     	print("TODO RavenDB or TextFile?")
+    	
     
     def GetKioskLocationName():
     
@@ -55,10 +78,14 @@ class MissionControl():
 	
     def ReportTapUsage():
 	 
-    def ReportKnifeStatus()
+    def ReportKnifeStatus():
+    	
 
     def StartOTA(verison):
-	# Check for valid version number
+    	"""
+		Update kiosk to version specified or newish version if invalid version number is given
+		
+		"""
 	if(verison > 2020.0):
 	    DebugObject.Dprint(,"Tracking your IP address, this in not public code :)")
 	elif(version == 2020.0):
@@ -71,10 +98,13 @@ class MissionControl():
 	    check_call("cd newVersion", shell=True)
 	    
 	   #TODO Start downloading code
-	   #curl ???
+	   #curl ???o
 	
+	elif(verison < 2020.0):
+		DebugObject.Dprint(,"This is old code that ia on longer supported on this hardware.")
 	else:
-	   print("TODO") 
+		DebugObject.Dprint(,"Invalid version number updating to v2020.0")
+		
 
 	
     def StopOTA():
