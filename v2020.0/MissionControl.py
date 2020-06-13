5e83395e710b3c7fd70912a35fd5c165a9097c0f
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-06-13 07:16:15 -0700

    WIP: Added more function documentation

--- v2020.0/MissionControl.py
+++ v2020.0/MissionControl.py
@@ -5,7 +5,7 @@ __author__  = "Blaze Sanders"
 __email__   = "blaze.d.a.sanders@gmail.mvp"
 __company__ = "Robotic Beverage Technologies, Inc"
 __status__  = "Development"
-__date__    = "Late Updated: 2020-05-12"
+__date__    = "Late Updated: 2020-06-13"
 __doc__     = "Class to define OTA commuications architecture of 30K+ Tapomatic kiosk"
 """
 
@@ -26,6 +26,9 @@ class MissionControl():
 
 	totalCoconutsVended = 0
 	totalTapsUsed = 0
+	totalFlavorOzUsed = 0
+	totalHealthOzUsed = 0
+	totalCoirFiberRemoved = 0
 	
 
     def __init__(self, kioskID, version, key):
@@ -37,7 +40,7 @@ class MissionControl():
 		key -- Security key for ALL Tapoamtics to allow Over-The-Air (OTA) updates
 		
 		Return value:
-		Newly created MissionControl() object
+		New MissionControl() object
 		"""
 
         self.DebugObject = Debug(True)
@@ -48,39 +51,127 @@ class MissionControl():
 
     def ReportLiquidLevel(lType, internalBottleLocation, kioskID):
     	"""
-    	Report the current liquid level as perfectage 
+    	Report the current liquid level as percentage 
     	
+    	Key arguments:
+        lType -- Type of liquid add-on to inject into thr coconut
+        internalBottleLocation -- Position starting with 0 that a bottle is from the left side of kiosk as you move right
+kioskID -- Unique ID for every prototype or production Tapomatic manufactured
+
+    	Return value:
+    	liqPercentage -- Percent that a 750 mL botle is full
     	"""
     	
-    	
+    	return liqPercentage
     	
     	
     	
     def ReportLowLiquidLevel(lType, internalBottleLocation, kioskID):
     	"""
     	High priority alert that the current liquid level is below 20%
+    	    	
+    	Key arguments:
+        lType -- Type of liquid add-on to inject into thr coconut
+        internalBottleLocation -- Position starting with 0 that a bottle is from the left side of kiosk as you move right
+kioskID -- Unique ID for every prototype or production Tapomatic manufactured
+
+    	Return value:
+    	liqPercentage -- Percent (less then 20%) that a 750 mL botle is full
     	"""
+    	
+    	
+    	return liqPercentage
     
     def GetKioskGPSlocation(kioskID):
     	"""
+    	Determine the GPS location using celluar towers (not a GPS satilite receiver) or a hard coded value in non-volitile memory 
+    	
+    	Key arguments:
+    	kioskID -- Unique ID for every prototype or production Tapomatic manufactured
+    	
+    	Return value:
+    	gpsData -- Lattitude, Longitude, and Altitude data 
+    	
     	Update to RavenDB Implemention in August 20200
     	"""
+    	
+    	# Hard coded locations that have TERRIBLE cell service 
+    	try:
+    		f = open(kioskLocation.csv, 'rb')  # open only in read mode.
+    		data = f.read(DATA_BUFFER_SIZE) # Read for Buffer Size.
+    	except:
+    		this.DebugObject.Dprint("Could not open {}, ensure the filepath is correct.")
+    	
+    	
     	print("TODO RavenDB or TextFile?")
     	
     
     def GetKioskLocationName():
-    
+        """
+    	
+    	
+    	Key arguments:
+    	
+    	Return value:
+    	
+    	"""
+    	
+    	
     def ReportDamage():
-	
+	    """
+    	
+    	
+    	Key arguments:
+    	
+    	Return value:
+    	
+    	"""
+    	
     def ReportPowerState():
-	
-    def ReportCoconutUsage():
+    	"""
+    	
+    	
+    	Key arguments:
+    	
+    	Return value:
+    	
+    	"""
 	
     def ReportTapUsage():
-	 
-    def ReportKnifeStatus():
+    	"""
     	
-
+    	Return value:
+    	totalTapsUsed -- Total number of CoirTek taps removed from tap ring since last count. Power cycling machine should NOT reset this variable.
+    	"""
+    	
+    	return totalTapsUsed
+	
+    def ReportCoconutUsage():
+		"""
+    	
+    	Return value:
+    	totalCoconutsUsed -- Total number coconuts tapped since last count. Power cycling machine should NOT reset this variable.
+    	"""
+    	
+    	return totalCoconutsUsed
+    
+    def ReportKnifeStatus():
+    	"""
+    	
+    	
+    	Key arguments:
+    	
+    	Return value:
+    	
+    	"""
+    	
+	def ConnectToDSDservive():
+		"""
+		Rob's sale CRM and accounting sysytem 
+		https://www.laceupsolutions.com/dsd-software-1
+		"""
+		
+		
     def StartOTA(verison):
     	"""
 		Update kiosk to version specified or newish version if invalid version number is given
@@ -101,12 +192,20 @@ class MissionControl():
 	   #curl ???o
 	
 	elif(verison < 2020.0):
-		DebugObject.Dprint(,"This is old code that ia on longer supported on this hardware.")
+		self.DebugObject.Dprint(,"This is old code that ia on longer supported on this hardware.")
 	else:
-		DebugObject.Dprint(,"Invalid version number updating to v2020.0")
+		self.DebugObject.Dprint(,"Invalid version number updating to v2020.0")
 		
 
 	
     def StopOTA():
+    	"""
+    	
+    	
+    	Key arguments:
+    	
+    	Return value:
+    	
+    	"""
 	
 	
