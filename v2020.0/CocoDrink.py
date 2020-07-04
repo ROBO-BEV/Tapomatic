cc56312cca8f718dda47e562d67a0fc29dad8f53
Blaze Sanders <blazesanders@Blazes-MacBook-Pro.local>
2020-07-04 06:19:33 -0700

    WIP: Updated helper functions and programmatically found filename

--- v2020.0/CocoDrink.py
+++ v2020.0/CocoDrink.py
@@ -4,12 +4,15 @@ __author__ =  "Blaze Sanders"
 __email__ =   "blaze.d.a.sanders@gmail.com"
 __company__ = "Robotic Beverage Technologies Inc"
 __status__ =  "Development"
-__date__ =    "Late Updated: 2020-06-29"
-__doc__ =     "Class to define flavors and health additivies in a CocoDrink"
+__date__ =    "Late Updated: 2020-07-03"
+__doc__ =     "Class to define flavors and health additivies in a CocoTaps coconut drink"
 """
 
 # Robotic Beverage Technologies code for custom data logging and terminal debugging output
 from Debug import *
+
+# Allow program to extract filename of the current file
+import os
 
 try:
 	# Robotic Beverage Technologies code for controlling many different types of motors, servos, and relays
@@ -22,6 +25,7 @@ except ImportError:
 	#TODO DO LOW LEVEL PIN CONTROL THAT WORKS EVER WHERE? http://wiringpi.com/the-gpio-utility/
 	TempDebugObject = Debug(True, "ImportError Try/Catch")
 	TempDebugObject.Dprint("WARNING: You are running code on Mac or PC (NOT a Raspberry Pi 4), thus hardware control is not possible.")
+
 
 class CocoDrink:
 
@@ -52,7 +56,7 @@ class CocoDrink:
 
 	# User GUI.py program flow selection CONSTANTS
 	TAPPED = 0
-	TOPPED_OFF = 1  #TODO REMOVE - LIKELY NOT GOING TO BE SUPPORTED IN v2020.2
+	TOPPED_OFF = 1  #TODO REMOVE - LIKELY NOT GOING TO BE SUPPORTED IN v2020.1
 	FLAVOR = 2
 	HEALTH_ADDITTIVE = 3
 
@@ -67,7 +71,7 @@ class CocoDrink:
 
 	def __init__(self, drinkName, addOnFlavor, addOnFlavorLevel, addOnHealthAdditive, addOnHealthAdditiveLevel, tapOrCutFlow, brandingArt):
 		"""
-		Constructor to initialize an CocoDrink object
+		Constructor to initialize an CocoDrink() object, which stores add-ons and artwork
 
 		Key arguments:
 		self -- Newly created CocoDrink object
@@ -85,14 +89,15 @@ class CocoDrink:
 		New CocoDrink() object
 		"""
 
-		self.DebugObject = Debug(True, "CocoDrink.py")
-
+        currentProgramFilename = os.path.basename(__file__)
+		self.DebugObject = Debug(True, currentProgramFilename) 
+        
 		self.drinkName = drinkName
 		self.addOnFlavor = addOnFlavor
 		self.addOnFlavorLevel = addOnFlavorLevel
 		self.addOnHealthAdditive = addOnHealthAdditive
 		self.addOnHealthAdditiveLevel = addOnHealthAdditiveLevel
-		self.tapOrCutFlow = tapOrCutFlow
+		self.tapOrCutFlow = tapOrCutFlow   	                        #TODO REMOVE - LIKELY NOT GOING TO BE SUPPORTED IN v2020.1
 		self.brandingArt = brandingArt
 
 		if(addOnFlavorLevel > CocoDrink.MAX_ADD_ON_LEVEL or addOnHealthAdditiveLevel > CocoDrink.MAX_ADD_ON_LEVEL):
@@ -115,10 +120,10 @@ class CocoDrink:
 		https://stackoverflow.com/questions/1984325/explaining-pythons-enter-and-exit
 
 		Key arguments:
-		self -- 
-
+        NONE
+        
 		Return value:
-		???
+		NOTHING
 		"""
 
 		print("in __enter__")
@@ -136,7 +141,7 @@ class CocoDrink:
 		traceback --
 
 		Return value:
-		???
+		NOTHING
 		"""
 
 		self.DebugObject.Dprint("Cleaning up CocoDrink() object in __exit__ ")
@@ -150,27 +155,29 @@ class CocoDrink:
 		lType -- Type of liquid add-on to inject into a coconut
 
 		Return value:
-		addOn - CocoDrink CONSTANT based on user selection
+		addOn -- CocoDrink CONSTANT based on user selection
 		"""
 
 		if(lType == FLAVOR):
-			addOn = GetFlavorType(self)
+			addOn = self.GetFlavorType()   #TODO Is this correct call format???
 		elif(lType == HEALTH_ADDITTIVE):
-			addOn = GetHealthAdditiveType(self)
+			addOn = GetHealthAdditiveType(self) #TODO OR is this???
 
 		return addOn
-
-
-#	def GetFlavorType(self):
-#		Get product name of flavoring
-#
-#		Key arguments:
-#		self -- Instance of object being called
-#
-#		Return value:
-#		Integer GLOBAL CONSTANT from CocoDrink.py
-#
-#		return self.addOnFlavor
+		
+		
+	def GetFlavorType(self):
+	    """
+	    Get product name of flavoring
+	    
+	    Key arguments:
+	    NONE
+	    
+	    Return value:
+	    Integer GLOBAL CONSTANT from CocoDrink.py
+	    """
+	    
+	    return self.addOnFlavor
 
 
 	def GetHealthAdditiveType(self):
@@ -178,8 +185,8 @@ class CocoDrink:
 		Get product name of additive fluid
 
     	Key arguments:
-    	self -- Instance of object being called
-
+        NONE
+        
     	Return value:
     	return Integer GLOBAL CONSTANT from CocoDrink.py
 		"""
@@ -192,7 +199,7 @@ class CocoDrink:
 		Get filename of logo LASER branding into coconut
 
     	Key arguments:
-    	self -- Instance of object being called
+    	NONE
 
     	Return value:
     	return String GLOBAL CONSTANT from CocoDrink.py
@@ -206,75 +213,108 @@ class CocoDrink:
 		Get amount / level of flavor fluid in a coco drink in User Interface units
 
     	Key arguments:
-    	self -- Instance of object being called
-
+        NONE
+        
     	Return value:
     	return Integer value between 0 and  MAX_ADD_ON_LEVEL inclusively
 		"""
 
 		return self.addOnFlavorLevel
 
-	def ConvertLevelToVolume(self, addOnUnits):
-		"""
-		Since each fluid has a different flavor or health effect strength per mL this function defined what the User Interface units of mean
-
-		Key arguments:
-		addOnUnits -- Integer between and 0 and MAX_ADD_ON_LEVEL (inclusive)
-
-		Reture value:
-		volume - In units of mL for pumps to dispense
-		"""
-
-		if(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilliLiterFactor = 15
-		elif(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilliLiterFactor = 13.3
-		elif(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilloiiterFactor = 12
-		elif(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilliLiterFactor = 19
-		elif(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilliLiterFactor = 3
-		elif(self.drinkName == IMMUNITY_BOOST):
-			oneUnitToMilliLiterFactor = 9
-		else:
-			self.DebugObject.Dprint(self.DebugObject, "ERROR CODE (0x" + GLOBAL_CONSTANT_USAGE_ERROR + ") Verfiy that this Tapomatic unit has " + self.drinkName + " stocked.")
-
-		volume = addOnUnits * oneUnitToMilliLeterFactor
-		return volume
-
-
-	def ConvertVolumeToPumpRunTime(volume, pumpPartNumber):
-		thiccBoiFactor = 1 # Set to default value
-
-		if(pumpPartNumber == "202-0006-A"):
-			oneMilloLiterToSecondFactor = 2.2
-		else:
-			Debug.Dprint(self.DebugObject, "ERROR CODE (0x" + PUMP_CONFIGURATION_ERROR + ") Verify that you are using the correct pump part nuumber, as defined in Actuator.py and CocoDrink.py")
-
-		if(self.drinkName == PINA_COLADA):
-			thiccBoiFactor = 1.75			# This flavoring is extra thick, pump longer to get correct volume
-
-		seconds = volume * oneMilloLiterToSecondFactor * thiccBoiFactor
-		return seconds
-
-
 	def GetHealthAdditiveLevel(self):
 		"""
-		Get amount / level of health additive fluid in a coco drink in User Interface units
+		Get amount / level of health additive fluid in a Cocodrink in User Interface units
 
     	Key arguments:
-    	self -- Instance of object being called
-
-	    Return value:
-	    return Integer value between 0 and  MAX_ADD_ON_LEVEL (inclusive)
+        NONE
+        
+    	Return value:
+    	return Integer value between 0 and  MAX_ADD_ON_LEVEL inclusively
 		"""
 
 		return self.addOnHealthAdditiveLevel
 
 
-if __name__ == "__main__":
-	CocoDrinkDebugObject = Debug(True, "CocoDrink.py")
-	CocoDrinkDebugObject.Dprint("Running CocoDrink.py main unit test")
+	def ConvertLevelToVolume(self, addOnUnits):
+		"""
+		Since each fluid has a different flavor or health effect strength per mL this function defined what the User Interface units represent
 
-	TestCocoDrinkObject = CocoDrink(CocoDrink.COCONUT, CocoDrink.NONE, 0, CocoDrink.DAILY_VITAMINS, 5, CocoDrink.TAPPED, CocoDrink.COCOTAPS_LOGO)
+		Key arguments:
+		addOnUnits -- Integer variable, between and 0 and MAX_ADD_ON_LEVEL (inclusive)
+
+		Reture value:
+		volume -- Interger variable, in units of mL for pumps to dispense
+		"""
+
+		self.addOnFlavor = addOnFlavor
+		self.addOnFlavorLevel = addOnFlavorLevel
+		self.addOnHealthAdditive = addOnHealthAdditive
+		self.addOnHealthAdditiveLevel = addOnHealthAdditiveLevel
+
+
+		if(self.addOnHealthAdditive == IMMUNITY_BOOST):
+			oneUnitToMilliLiterFactor = 15
+		elif(self.addOnHealthAdditive == DAILY_VITAMINS):
+			oneUnitToMilliLiterFactor = 13.3
+		elif(self.addOnHealthAdditive ==ENERGY_BOOST):
+			oneUnitToMilliLiterFactor = 13.3
+
+		elif(self.addOnFlavor == PINA_COLADA):
+			oneUnitToMilloiiterFactor = 12
+		elif(self.addOnFlavor == PINEAPPLE_FLAVOR):
+			oneUnitToMilliLiterFactor = 19
+		elif(self.addOnFlavor == ORANGE_FLAVOR):
+			oneUnitToMilliLiterFactor = 3
+			
+		else:
+			self.DebugObject.Dprint("ERROR CODE (0x" + GLOBAL_CONSTANT_USAGE_ERROR + ") Verfiy that this Tapomatic unit has " + self.addOnHealthAdditice + " & " + self.addOneFlavor  + " stocked.")
+
+		volume = addOnUnits * oneUnitToMilliLeterFactor
+		
+		return volume
+
+
+	def ConvertVolumeToPumpRunTime(volume, cocoPartNumber):
+	    """
+		Since motor models can vary in dispense volume / sec this function adjusts run time 
+        
+        Key argument:
+        volume -- Interger variable, of desired volume to be dispensed
+        cocoPartNumber -- String variable, which is internal CocoTaps data in the format (XXX-YYYY-ZZ)) 
+        
+        Return value:
+        seconds - Float variable, that pump should run in order to dispense the correct volume of liquid
+        """
+
+	    thiccBoiFactor = 1                      # Set to default value 
+	    
+	    if(pumpPartNumber == "202-0006-A"):
+	        oneMilloLiterToSecondFactor = 2.2
+	    else:
+	        this.DebugObject.Dprint("ERROR CODE (0x" + PUMP_CONFIGURATION_ERROR + ") Verify that you are using the correct pump part nuumber, as defined in Actuator.py and CocoDrink.py")
+
+	    if(self.drinkName == PINA_COLADA):
+	        thiccBoiFactor = 1.75			# This flavoring is extra thick, pump longer to get correct volume
+
+	    seconds = volume * oneMilloLiterToSecondFactor * thiccBoiFactor
+	    
+	    return seconds
+
+	
+if __name__ == "__main__":
+
+    try:
+        UnitTest()
+    except NameError:
+        print("UnitTest() failed - Have a nice day :)")            
+    
+    print("END CocoDrink.py MAIN")
+
+
+def UnitTest():
+    print("START CocoDrink.py UnitTest()")
+    
+    TestCocoDrinkObject = CocoDrink(CocoDrink.COCONUT, CocoDrink.NONE, 0, CocoDrink.DAILY_VITAMINS, 5, CocoDrink.TAPPED, CocoDrink.COCOTAPS_LOGO)
+
+    print("END CocoDrink.py UnitTest()")
+    
