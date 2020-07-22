fac3dc19786947a329725069b237ba20be5c1681
dull7295 <jeevanmurali@gmail.com>
2020-07-22 18:38:11 -0400

    Refractored WEBCAM Code.

--- v2020.0/WebCam.py
+++ v2020.0/WebCam.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-__author__ =  "Murali ???"
+__author__ =  "Muralikrishna Dulla"
 __email__ =   "m@beepbeeptechinc.com"
 __company__ = "CocoTaps"
 __status__ =  "Development"
-__date__ =    "Late Updated: 2020-07-19"
+__date__ =    "Late Updated: 2020-07-22"
 __doc__ =     "Class to create QR codes for drinks and control USB camera to scan them"
 """
 
@@ -19,105 +19,77 @@ from CocoDrink import *         # Store valid CoCoTaps drink configurations
 
 
 class WebCam:
-
 	# CocoTaps  URL
 	COCOTAPS_URL = 'https://www.cocotaps.com/'
+	# Directory To Save the Generated QR Codes.
+	QRCODES_DIR = os.getcwd() + '/QRCodes'
+	# Directory where Source Images to embed behind the QR code.
+	QRImages_DIR = 'static/QRImages/'
 
-	# QR Strings
-	# IMMUNITY_BOOST = 'IMMUNITY_BOOST'
-	# DAILY_VITAMINS = 'DAILY_VITAMINS'
-	# ENERGY_BOOST = 'ENERGY_BOOST'
-	# PINA_COLADA = 'PINA_COLADA'
-	# PINEAPPLE_FLAVOR = 'PINEAPPLE_FLAVOR'
-	# ORANGE_FLAVOR = 'ORANGE_FLAVOR'
+	# Source Images to embed behind the QR code.
+	IMMUNITY_BOOST_IMG = 'immunity_boost.png'
+	DAILY_VITAMINS_IMG = 'daily_vitamins.jpg'
+	ENERGY_BOOST_IMG = 'energy_boost.jpg'
+	PINA_COLADA_IMG = 'pina_colada.jpg'
+	PINEAPPLE_FLAVOR_IMG = 'pineapple_flavor.jpg'
+	ORANGE_FLAVOR_IMG = 'orange_flavor.jpg'
 
-	QRCODES_DIR = os.getcwd()+'/QRCodes'
+	# Dictionary to store the mappings of cocodrink codes with the images to embed behind the QR code.
+	qrDictionary = {}
+	qrDictionary[CocoDrink.IMMUNITY_BOOST]   =    IMMUNITY_BOOST_IMG
+	qrDictionary[CocoDrink.DAILY_VITAMINS]   =    DAILY_VITAMINS_IMG
+	qrDictionary[CocoDrink.ENERGY_BOOST]     =    ENERGY_BOOST_IMG
+	qrDictionary[CocoDrink.PINA_COLADA]      =    PINA_COLADA_IMG
+	qrDictionary[CocoDrink.PINEAPPLE_FLAVOR] =    PINEAPPLE_FLAVOR_IMG
+	qrDictionary[CocoDrink.ORANGE_FLAVOR]    =    ORANGE_FLAVOR_IMG
 
 
-	def generateQR(self, QRword, picture):
+	def generateQR(self, qrWord, picture):
+			"""
+			Generate a word based  MYQR code
+			Key arguments:
+			QRword -- Data to encode into QR code dots
+			picture -- Background image to embed behind QR code dots
+
+			Return value:
+			None
+			"""
+			version, level, qr_name = myqr.run(
+				qrWord,
+				version=10,
+				level='H',
+				picture=picture,
+				colorized=True,
+				contrast=1.5,
+				brightness=1.6,
+				save_name=None,
+				save_dir=self.QRCODES_DIR
+			)
+			return
+
+
+	def generateQRCode(self, drinkCode):
 		"""
-		Generate a word based  MYQR code 
-		
-		Key arguments:
-		QRword -- Data to encode into QR code dots
-		picture -- Background image to embed behind QR code dots 
-		
-		Return value:
-		??? --
-		"""
-		
-		version, level, qr_name = myqr.run(
-			QRWord,
-			version=10,
-			level='H',
-			picture=picture,
-			colorized=True,
-			contrast=1.5,
-			brightness=1.6,
-			save_name=None,
-			save_dir=self.QRCODES_DIR
-		)
-		
-		return
-
-
-	def generateQRCodeForImmunityBoost(self):
-		"""
-		I think we can delete all these functions and add swtich statement to main generateQR function -BLAZE
-		
-		Generate QR code with CONSTANT INT IMMUNITY_BOOST encoded into dots
-		
+		Generate QR code with CONSTANT INT Drink Code and embed the mapped source images behind the code.
 		Key argmuments:
-		NONE
-		
+		drinkCode: CocoDrink Code.
 		Return value:
-		immunity_boost.png -- Image created and stored in "static/QRImages" directory 
+		None.
 		"""
-		
-		self.generateQR(CocoDrink.IMMUNITY_BOOST,'static/QRImages/'+'immunity_boost.png')
-		
+		qrImage = self.QRImages_DIR + self.qrDictionary.get(drinkCode)
+		qrWord  = self.COCOTAPS_URL  + str(drinkCode)
+		self.generateQR(qrWord, qrImage)
 		return
-
-
-	def generateQRCodeForDailyVitamins(self):
-		"""
-		Adding URL makes data more ???
-		
-		"""
-		self.generateQR(self.COCOTAPS_URL + CocoDrink.DAILY_VITAMINS,'static/QRImages/'+'daily_vitamins.jpg')
-		return          #WHY EMPTY RETURN
-
-	def generateQRCodeForEnergyBoost(self):
-		self.generateQR(self.COCOTAPS_URL + CocoDrink.ENERGY_BOOST,'static/QRImages/'+'energy_boost.jpg')
-		return
-
-	def generateQRCodeForPinaColada(self):
-		self.generateQR(self.COCOTAPS_URL + CocoDrink.PINA_COLADA,'static/QRImages/'+'pina_colada.jpg')
-		return
-
-	def generateQRCodeForPineappleFlavor(self):
-		self.generateQR(self.COCOTAPS_URL + CocoDrink.PINEAPPLE_FLAVOR,'static/QRImages/'+'pineapple_flavor.jpg')
-		return
-
-	def generateQRCodeForOrangeFlavor(self):
-		self.generateQR(self.COCOTAPS_URL + CocoDrink.ORANGE_FLAVOR,'static/QRImages/'+'orange_flavor.jpg')
-		return
-
 
 if __name__ == "__main__":
 	
 	object = WebCam()
-
-	object.generateQRCodeForImmunityBoost()
-	object.generateQRCodeForDailyVitamins()
-	object.generateQRCodeForEnergyBoost()
-	object.generateQRCodeForPinaColada()
-	object.generateQRCodeForPineappleFlavor()
-	object.generateQRCodeForOrangeFlavor()
-
-
-    testObject2 = WebCam()
-	testObject2.generateQR(CocoDrink.ORANGE_FLAVOR,'static/QRImages/'+'orange_flavor2.jpg')
+	## TODO : Blaze you can use this code block to generate the QR codes.
+	list = [CocoDrink.ORANGE_FLAVOR, CocoDrink.PINEAPPLE_FLAVOR, CocoDrink.PINA_COLADA, CocoDrink.ENERGY_BOOST, CocoDrink.IMMUNITY_BOOST, CocoDrink.DAILY_VITAMINS]
+	for i in list:
+		object.generateQRCode(i)
+	# testObject2 = WebCam()
+	# testObject2.generateQR(CocoDrink.ORANGE_FLAVOR,'static/QRImages/'+'orange_flavor2.jpg')
 
 
 
