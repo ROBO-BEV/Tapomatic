dfc49dea68a6b3991606901239408216b0703d3b
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-07-31 16:00:27 -0700

    WIP: Fixing imperial and metric function calling order

--- v2020.0/Ping.py
+++ v2020.0/Ping.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
 __author__  = "Blaze Sanders"
-__email__   = "blaze.d.a.sanders@gmail.mvp"
+__email__   = "blaze.d.a.sanders@gmail.com"
 __company__ = "Robotic Beverage Technologies Inc"
 __status__  = "Development"
-__date__    = "Late Updated: 2020-04-17"
+__date__    = "Late Updated: 2020-07-29"
 __doc__     = "Class to operate up to 8 PING LASER Or Ultrasonic rangefinders from Parallax Inc"
 """
 
@@ -14,16 +14,20 @@ __doc__     = "Class to operate up to 8 PING LASER Or Ultrasonic rangefinders fr
 # https://gpiozero.readthedocs.io/en/stable/api_input.html
 
 # Allow program to pause operation and create local timestamps
-import time 
+import time
 
-# Robotic Beverage Technologies code for custom data logging and terminal debugging output
-from Debug import *
+# Allow program to extract filename of the current file
+import os
+
+# Custom CocoTaps and Robotic Beverage Technologies Inc code
+from Debug import *             # Configure datalogging parameters and debug printing control
+from RaspPi import *            # Contains usefull GPIO pin CONSTANTS and setup configurations
 
 # PING type Global CONSTANTS
 LASER = 0
 ULTRASONIC = 1
 
-# PING type Global Variable 
+# PING type Global Variable
 pingType = -1
 
 try:
@@ -34,7 +38,7 @@ try:
 
 	# Allow asynchrous event to occur in parallel and pause threads as needed
 	# Might work on Windows in the future https://github.com/vibora-io/vibora/issues/126
-	from signal import pause 
+	from signal import pause
 
 	# Allow control of input and out pins
 	from gpiozero import OutputDevice, InputDevice
@@ -49,52 +53,52 @@ try:
 	from gpiozero import TimeOfDay
 
 except ImportError:
-	DebugObject = Debug(True)
-	Debug.Dprint(DebugObject, "WARNING: You are running code on Mac or PC (NOT a Raspberry Pi 4), thus hardware control is not possible.")
+	currentProgramFilename = os.path.basename(__file__)
+	TempDebugObject = Debug(True, "Try/Catch ImportError in " + currentProgramFilename)
+	TempDebugObject.Dprint(DebugObject, "WARNING: You are running code on Mac or PC (NOT a Raspberry Pi 4), thus hardware control is not possible.")
 
 
 def GetMillimeters(pin):
     """
     Get distance to closest object in direct line of sight for PING LASER rangefinder
-    
+
     Key arguments:
     pin -- GPIO pin PING sensor is connected to
-    
-    Return value:
-    range -- Distance in mm to closest object 
+
+    Return value: range -- Distance in mm to closest object
     """
-    range = distance(pin)
-    
+
+    range = 10 * distance(pin)
+
     return range
 
- 
+
 def GetInches(pin):
     """
     Calculate distance to closet object using Get Millimeters() function above. Metric FTW!
-    
+
     Key arguments:
     pin -- GPIO pin PING sensor is connected to
-    
+
     Return value:
     range - Distance in gross imperial units to closest object
     """
-    
-    range = 25.4 * GetMillimeters(pin) 
-    
+
+    range = 25.4 * GetMillimeters(pin)
+
     return range
 
 
- 
 def distance(pin):
     """
     Measure distance to the closest object using the time of flight of a LASER or ULTRASONIC pulse 
-    
+
     Key arguments:
     pin -- GPIO pin PING sensor is connected to
     pingType -- Either LASER or ULTRASONIC
-    
+
     Return value:
-    distance -- 
+    distance --
     """
 
     # Set GPIO Pin for PING sensors (Only one of three axis is powered at a time to reduce LASER and ultrasonic cross talk)
@@ -107,23 +111,23 @@ def distance(pin):
     # set Trigger to HIGH
     GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
     GPIO.output(GPIO_TRIGGER, True)
- 
+
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
     GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
+
     StartTime = time.time()
     StopTime = time.time()
- 
+
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
- 
+
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
- 
+
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s) or TODO light speed (30000000000 cm/s)
@@ -132,22 +136,25 @@ def distance(pin):
         distance = (TimeElapsed * 30000000000) / 2
     else:
         distance = (TimeElapsed * 34300) / 2
- 
+
     return distance
-    
+
+
     def SetPingType(type):
         pingType = type
-        
+
+
     def GetPingType():
         return pingType
- 
+
+
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
+            dist = distance(4) #BOARD7 = GPIO4
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
- 
+
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
