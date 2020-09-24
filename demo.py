a6fcb71440815179bcf37820b3e5dad6f14608da
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-09-24 15:06:40 -0700

    Added simple code for demo of valves

---
+++ demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import  RPi.GPIO as GPIO
+import time
+
+HIGH = 1
+LOW = 0
+
+
+if __name__ == "__main__":
+	GPIO.setmode(GPIO.BOARD)
+
+	#GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+	#GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
+	##GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+
+	#GPIO.output(7, HIGH)
+	#GPIO.output(15, HIGH)
+	GPIO.output(31, HIGH)
+	##GPIO.output(7, HIGH)
+
+	time.sleep(3)
+
+	##GPIO.cleanup(7)
+	##GPIO.cleanup(15)
+	##GPIO.cleanup(19)
+	##GPIO.cleanup(22)
+
+	GPIO.output(31, LOW)
