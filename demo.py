741c2603764c2af2cc02708a3ae51adb60cb0239
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-09-24 16:34:01 -0700

    Finish Demo.py for valve control and added serial install library

--- demo.py
+++ demo.py
@@ -10,15 +10,15 @@ LOW = 0
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
 
-	#GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 	#GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
-	GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
-	##GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+	#GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)
 
-	#GPIO.output(7, HIGH)
+	GPIO.output(7, HIGH)
 	#GPIO.output(15, HIGH)
-	GPIO.output(31, HIGH)
-	##GPIO.output(7, HIGH)
+	#GPIO.output(31, HIGH)
+	GPIO.output(37, HIGH)
 
 	time.sleep(3)
 
@@ -27,4 +27,5 @@ if __name__ == "__main__":
 	##GPIO.cleanup(19)
 	##GPIO.cleanup(22)
 
-	GPIO.output(31, LOW)
+	GPIO.output(7, LOW)
+	GPIO.output(37, LOW)
