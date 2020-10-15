b0fbdf71948b1c8f24ae8541b4648232d07213ca
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-10-15 15:36:18 -0700

    Add LASER CV code to demo.py

--- demo.py
+++ demo.py
@@ -2,12 +2,47 @@
 
 import  RPi.GPIO as GPIO
 import time
+import numpy
+
+import cv2
+#from CocoDrink import *
 
 HIGH = 1
 LOW = 0
 
+def LoadColorImage():
+	path = '/home/pi/Tapomatic/v2020.0/static/images/BacardiLogoV1.png'
+	originalImage = cv2.imread(path)
+
+	print('Dimensions: ', originalImage.shape)
+	imgHeight, imgWidth, colorChannels = originalImage.shape
+	print('Height: ', imgHeight)
+	print('Width: ', imgWidth)
+	print('Color Channels: ', colorChannels)
+
+	#cv2.imshow('image',originalImage)
+
+	return originalImage
+
+def LoadGrayScaleImage():
+	path = '/home/pi/Tapomatic/v2020.0/static/images/BacardiLogoV1.png'
+	grayScaleImage = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+	print('Dimensions: ', grayScaleImage.shape)
+	imgHeight, imgWidth =  grayScaleImage.shape
+	print('Height: ', imgHeight)
+	print('Width: ', imgWidth)
+
+	#cv2.imshow('image',originalImage)
+
+	return grayScaleImage
+
 
 if __name__ == "__main__":
+
+	LoadColorImage()
+	LoadGrayScaleImage()
+
 	GPIO.setmode(GPIO.BOARD)
 
 	GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
@@ -22,10 +57,7 @@ if __name__ == "__main__":
 
 	time.sleep(3)
 
-	##GPIO.cleanup(7)
+	GPIO.cleanup()
 	##GPIO.cleanup(15)
-	##GPIO.cleanup(19)
-	##GPIO.cleanup(22)
-
-	GPIO.output(7, LOW)
-	GPIO.output(37, LOW)
+	##GPIO.cleanup(31)
+	##GPIO.cleanup(37)
